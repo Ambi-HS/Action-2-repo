a0e36c06dafff6b77f97f12312a6be8f480eb494
Ambi-HS <ambikas@devtools.in>
2025-07-17 17:27:49 +0530

    editing ascii file

--- ascii-shell-script-2.sh
+++ ascii-shell-script-2.sh
@@ -3,3 +3,4 @@ cowsay -f dragon "dragon for cover, I am a dragon....rawr" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
  ls -ltra
+ #adding a new comment just for testing activites and filters
